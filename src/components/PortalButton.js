6d95a1a7b7484b609db07dda70e9ab4c65934c18
Kevin Rajgor <kevinrajg95@gmail.com>
2023-02-05 16:07:34 +0530

    Updated

--- src/components/PortalButton.js
+++ src/components/PortalButton.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 //complete this function using portals
-const PortalButton=()=>{
-    return (
-        <div>
-            <button id="button" onClick={}>Click</button>
-        </div>
-        
-    )
-}
-export default PortalButton;
+const PortalButton = (params) => {
+  return ReactDOM.createPortal(
+    <button id="button" onClick={params.buttonclick}>
+      Click
+    </button>,
+    document.getElementById("portal-button"),
+  );
+  //     return (
+  //         <div>
+  //             <button id="button" onClick={}>Click</button>
+  //         </div>
 
-//portal-button id will be used here for portal purpose
+  //     )
+};
+export default PortalButton;
